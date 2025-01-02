efff51b65e337ed265bf63868e457e8628ad7ede
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-01-02 03:46:21 -0300

    Actualizacion: 02/01 03:16 - Modificación app.py y requirements.txt

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def handle_exception(e):
     return {"mensaje": "Error interno del servidor", "tipo": "error"}, 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
