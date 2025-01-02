66a3942409ba0d8f9cebaa9a32d3c665d9505aeb
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-01-02 05:32:22 -0300

    Actualizacion: 02/01 03:16 - Proyecto desplegado.

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def handle_exception(e):
     return {"mensaje": "Error interno del servidor", "tipo": "error"}, 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
